02940a90363739572602ba931697edbc66e6ff64
crispim <crispim1411@gmail.com>
2022-01-30 13:29:18 -0300

    iterators

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,7 @@ use std::process;
 use minigrep::Config;
 
 fn main() {
-    //obtem argumentos passados
-    let args: Vec<String> = env::args().collect();
-    
-    //configuração a partir dos argumentos
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
